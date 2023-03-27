5d9037e75ab9cc9934ed7711b9b121390a4191b4
Macamostro <128429322+Macamostro@users.noreply.github.com>
2023-03-27 20:15:06 -0300

    Update respuestas.sql

--- parte-1/homework/respuestas.sql
+++ parte-1/homework/respuestas.sql
@@ -1,7 +1,61 @@
---1
-select * from stg.product_master
+--1. Mostrar todos los productos dentro de la categoria electro junto con todos los detalles.
+select * 
+from stg.product_master_alt
 where categoria = 'Electro'
 
---2
-select * from stg.product_master
-where origen = 'China'
+--2. Cuales son los producto producidos en China?
+select *
+from stg.product_master_alt
+where origen='China'
+
+--3. Mostrar todos los productos de Electro ordenados por nombre.
+select *
+from stg.product_master_alt
+where categoria = 'Electro'
+order by nombre
+
+--4. Cuales son las TV que se encuentran activas para la venta?
+select *
+from stg.product_master_alt
+where subcategoria = 'TV' and is_active = 'TRUE'
+
+--5. Mostrar todas las tiendas de Argentina ordenadas por fecha de apertura de las mas antigua a la mas nueva.
+select *
+from stg.store_master
+order by fecha_apertura asc 
+
+--6. Cuales fueron las ultimas 5 ordenes de ventas?
+select *
+from stg.order_line_sale
+limit 5
+
+--7. Mostrar los primeros 10 registros de el conteo de trafico por Super store ordenados por fecha.
+select *
+from stg.super_store_count
+order by fecha asc
+limit 10
+
+--8. Cuales son los producto de electro que no son Soporte de TV ni control remoto.
+select * 
+from stg.product_master_alt
+where categoria = 'Electro'  and subsubcategoria not in ('Soporte', 'Control remoto')
+
+--9. Mostrar todas las lineas de venta donde el monto sea mayor a $100.000 solo para transacciones en pesos.
+select * 
+from stg.order_line_sale
+where moneda = 'ARS' and venta > '100000'
+
+--10. Mostrar todas las lineas de ventas de Octubre 2022.
+select * 
+from stg.order_line_sale
+where fecha between '2022-10-01' and '2022-10-31'
+
+--11. Mostrar todos los productos que tengan EAN.
+select * 
+from stg.product_master_alt
+where ean is not null
+
+--12. Mostrar todas las lineas de venta que que hayan sido vendidas entre 1 de Octubre de 2022 y 10 de Noviembre de 2022.
+select * 
+from stg.order_line_sale
+where fecha between '2022-10-01' and '2022-11-10'
